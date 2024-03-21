146d8b2dfc4d8c4c00f9665954aca79b08f37887
Derik van Schaik <derikvanschaik@gmail.com>
2024-03-21 10:19:22 -0700

    make script accept input from stdin, fix minor bug when integrating that

--- kenken.py
+++ kenken.py
@@ -1,5 +1,17 @@
 import re
 from itertools import permutations
+import sys
+
+
+def extract_puzzle_dimension(puzzle_header):
+    pattern = r"#kenken www\.kenkenpuzzle\.com Puzzle (\d+) (\d+)x\2 (Easiest|Easy|Medium|Hard|Hardest)"
+    # Perform regex search
+    match = re.search(pattern, puzzle_header)
+
+    if match:
+        matrix_size = match.group(2)
+        return matrix_size
+    return None
 
 
 # parses raw ken ken, returns dict of form
@@ -11,11 +23,13 @@ def parse_rules(unparsed, n=7):
     pattern = re.compile(r"(r\d+)(?:\.(\d+)([+\-*/])?)?")
 
     variables = [f"V{num}" for num in range((n * n))]
+
     rules = []
     for line in unparsed.split("\n"):
         rs = line.split(",")
         for r in rs:
-            rules.append(r)
+            if r:
+                rules.append(r)
 
     rule_mapper = {}
 
@@ -155,44 +169,13 @@ def build_constraints(rules, n=7):
 
 if __name__ == "__main__":
 
-    ex = """r1.16+,r2.3+,r2,r3.2-,r3,r4.4-,r4
-r1,r1,r5.10+,r6.3+,r6,r7.7+,r7
-r8.5+,r9.12+,r5,r10.13+,r11.6-,r12.4-,r12
-r8,r9,r9,r10,r11,r13.9+,r13
-r14.4+,r14,r15.11+,r15,r16.4,r17.13+,r18.3+
-r19.11+,r20.6-,r21.12+,r22.5+,r22,r17,r18
-r19,r20,r21,r21,r23.3-,r23,r24.6"""
+    puzzle_input = puzzle_input = [line for line in sys.stdin]
+    puzzle_header = puzzle_input[0]
+    puzzle = "".join(puzzle_input[1:])
 
-    ex2 = """r1.14+,r2.1,r3.13+,r3,r4.5+,r4,r5.6+
-r1,r1,r6.4+,r7.11+,r8.9+,r9.7,r5
-r10.6+,r10,r6,r7,r8,r11.3+,r12.12+
-r13.9+,r13,r14.11+,r15.5+,r15,r11,r12
-r16.10+,r17.13+,r14,r18.3+,r19.12+,r19,r19
-r16,r17,r20.7+,r18,r21.9+,r21,r22.3
-r23.3+,r23,r20,r24.10+,r24,r25.10+,r25"""
+    n = int(extract_puzzle_dimension(puzzle_header))
+    parsed = parse_rules(puzzle, n=n)
 
-    ex3 = """r1.16+,r2.1-,r2,r3.5-,r3,r4.3/,r4,r5.13+,r5
-r1,r6.4/,r6,r7.3-,r8.45*,r8,r9.22+,r9,r5
-r1,r10.3+,r10,r7,r11.2-,r11,r9,r12.2-,r12
-r13.3/,r14.1-,r15.8-,r16.120*,r16,r16,r17.20*,r18.2-,r19.2
-r13,r14,r15,r20.2-,r20,r17,r17,r18,r21.17+
-r22.9+,r23.63*,r24.1-,r25.5-,r24,r26.1-,r27.48*,r21,r21
-r22,r23,r24,r28.5-,r28,r26,r27,r29.4/,r29
-r30.3,r31.432*,r32.20+,r32,r33.18+,r34.3+,r34,r35.13+,r36.2-
-r31,r31,r32,r32,r33,r33,r33,r34,r3"""
-
-    ex4 = """r1.10+,r2.3+,r2,r3.8+,r3,r4.18+,r4
-r1,r5.11+,r5,r6.3+,r6,r7.3,r4
-r8.12+,r9.5+,r10.12+,r10,r11.4,r12.9+,r12
-r8,r9,r10,r13.6+,r14.11+,r14,r15.6+
-r16.10+,r16,r17.9+,r13,r18.3+,r18,r15
-r19.7+,r19,r17,r20.13+,r20,r21.6+,r22.8+
-r23.7+,r23,r24.5,r25.7+,r25,r21,r22"""
-
-    n = 7
-    parsed = parse_rules(ex4, n=n)
-    # print(parsed)
-    # print(parsed)
     declared = declare_variables(n=n)
     constraints = build_constraints(parsed, n=n)
 
