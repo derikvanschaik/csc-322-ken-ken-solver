853005411ac33e4d2ced16583ce39be7901de547
Derik van Schaik <derikvanschaik@gmail.com>
2024-03-21 11:19:50 -0700

    remove int parse

--- smtout.py
+++ smtout.py
@@ -6,7 +6,7 @@ smt_output = "".join([line for line in sys.stdin])
 pattern = re.compile(r"\(V\d+\s(\d+)\)")
 
 values = pattern.findall(smt_output)
-values = [int(value) for value in values]
+values = [value for value in values]
 
 
 print("".join(values))
